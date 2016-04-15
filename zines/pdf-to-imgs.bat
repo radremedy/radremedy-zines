d96a87f0e104582c545ffea65c9d3e286dafb36a
Cate S <radioprotector@users.noreply.github.com>
2016-04-14 23:45:34 -0400

    updating PDF->jpeg conversion script

--- zines/pdf-to-imgs.bat
+++ zines/pdf-to-imgs.bat
@@ -11,7 +11,7 @@ IF EXIST "%~n1" RMDIR /S /Q "%~n1"
 MKDIR "%~n1"
 
 echo on
-convert -monitor -colorspace rgb -density 300x300 -quality 95 -resize x1500 "%1" "%~n1/page.jpg"
+convert -monitor -colorspace rgb -density 300x300 -quality 90 -resize 1198x1851^> "%1" "%~n1/page.jpg"
 convert -monitor -colorspace rgb -density 300x300 -quality 70 -resize 250 -crop 250x250+0+0 "%1" "%~n1/thumb.jpg"
 
 :end
